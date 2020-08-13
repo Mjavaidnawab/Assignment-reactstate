0207a6ebef3c7b1752f4d0b2f134071420cb5b07
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-08-13 22:20:01 +0400

    commit react assignment state

--- src/Room.js
+++ src/Room.js
@@ -1,5 +1,5 @@
 import React, {useState}  from 'react';
- 
+import './Room.css'
 
 function Room() {
     let [isLit, setLit] = useState(true);
@@ -8,11 +8,33 @@ function Room() {
         console.log("Button Clicked");
         setLit(!isLit);
     }
+    function updateLitOn()
+    {
+        console.log("On Button Clicked");
+        if (!isLit)
+        {setLit(!isLit);}
+    }
+    function updateLitOff()
+    {
+        console.log("Off Button Clicked");
+        if (isLit)
+            {setLit(!isLit);}
+    }
   
     return (
-      <div>
+      <div className ={"room " +(isLit? "lit":"dark")}>
+          <h1>BootCamp 2020 Class 4 : React State</h1>
+          <h2>Assignment - Add On Off Button</h2>
+          <h3>Submitted by Muhammad Javaid Nawab</h3>
           This is Room is : {isLit? "lit":"dark"}
-          <button onClick ={updateLit}>Toggle Light</button>
+         
+         
+          <br/>
+          <button onClick ={updateLitOn}>ON</button>
+          <br/>
+          <button onClick ={updateLitOff}>OFF</button>
+          <br/> <button onClick ={updateLit}>Toggle Light</button>
+      
       </div>
     );
   }
